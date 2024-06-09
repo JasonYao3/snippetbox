a032e3cf3b7cb90a389e42f06a7136442c721b59
Jason Yao <jasonyao.jy@gmail.com>
2024-06-09 17:19:15 -0400

    Add table-driven tests for the HumanDate() method

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -21,7 +21,11 @@ type templateData struct {
 }
 
 func humanDate(t time.Time) string {
-	return t.Format("2006-01-02 3:4:5 pm")
+	if t.IsZero() {
+		return ""
+	}
+
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
